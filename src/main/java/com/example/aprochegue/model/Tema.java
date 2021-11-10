f563e1ec2ce90c3bc0f850af491b086650401098
LucasAci <lucasqueiroz.aci@gmail.com>
2021-11-10 14:22:45 -0300

    @LucasAci[Task: Create model-postagem and relationship between  theme and post tables]

--- src/main/java/com/example/aprochegue/model/Tema.java
+++ src/main/java/com/example/aprochegue/model/Tema.java
@@ -1,12 +1,18 @@
 package com.example.aprochegue.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 /**
@@ -53,6 +59,18 @@ public class Tema {
 	@Size(min = 5, max = 255)
 	private String assunto;
 	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List <Postagem> postagem;
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
 	public Long getId() {
 		return id;
 	}
